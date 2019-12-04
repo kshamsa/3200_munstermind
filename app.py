7e648866a631df63a9eeb32c07010bbfcaf72e95
kshamsa <kionkshamsa@gmail.com>
2019-12-03 17:37:41 -0700

    First attempt.  Added in some sorting.

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
+import numpy as np 
+
 @app.route('/')
 def hello_world():
     return render_template('munstermind.html')
+
+
 
 @app.route('/checkguess', methods={'POST'})
 def checkguess():
@@ -13,10 +17,37 @@ def checkguess():
     print(request.json['enigma']) #print out the enigma to the console
     guess_list = request.json['guess']
     enigma_list = request.json['enigma']
-
+    print("TEST OUT DATA BELOW HERE/////////////////////////////")
+    print(guess_list)
+    print(enigma_list)
     #Hey student: your code here!!!!
 
-    hint = {'whitePegs':1, 'blackPegs':2} #create the hint as a dict
+    guess = np.asarray(guess_list)
+    enigma = np.asarray(enigma_list)
+    results = ["","","",""]
+    whitePegs = 0 #black is a match
+    blackPegs = 0 #color exists but its in the wrong index
+    index = 0
+
+    for s in guess:
+        if s == enigma[index]:
+            whitePegs = whitePegs + 1
+            results[index] = "blackPeg"
+        
+        index = index + 1
+    
+    for s in guess:
+        if s in enigma_list:
+            if results[enigma_list.index(s)] != "blackPeg":
+                results[enigma_list.index(s)] = "whitePeg"
+                whitePegs = whitePegs + 1
+
+    print("whitePegs")
+    print(whitePegs)
+    print("blackPegs")
+    print(blackPegs)           
+    
+    hint = {'whitePegs':whitePegs, 'blackPegs':blackPegs} #create the hint as a dict
     print("the hint:", hint) #print out the hint to the console
     return jsonify(hint) #return the dict as a json
 
